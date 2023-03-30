5bea15989bbfa8dc18769a411b46f2073b3fbddb
Yayokratos <aapadi305@gmail.com>
2023-03-29 17:41:38 -0700

    updated

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,25 +5,13 @@
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-int n;
-int a[5];
+int a[5] = { 98, 102, 402, -102, 298 };
 int *p;
 
-a[2] = 1024;
-p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+p = a + 2;
+*(p) += 0;
+printf("a[2] = %d\n", *(p));
 return (0);
 }
